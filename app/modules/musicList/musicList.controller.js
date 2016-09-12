cf44820973ed07f1dc8348bd89d061ed5211a201
Pablo H <hernandez.pah91@gmail.com>
2016-09-11 21:26:45 -0600

    Fix responsive support in main music list page.

--- app/modules/musicList/musicList.controller.js
+++ app/modules/musicList/musicList.controller.js
@@ -4,9 +4,9 @@
     angular.module('hangarSongs')
         .controller('musicList.controller', musicListController);
 
-    musicListController.$inject = ['music.service'];
+    musicListController.$inject = ['music.service', 'spotifyMusic.model', 'Spotify'];
 
-    function musicListController(musicService) {
+    function musicListController(musicService, spotifyMusicModel, spotifyService) {
         var vm = this;
 
         // Public functions
@@ -15,19 +15,26 @@
 
         // Public variables
         vm.musicServiceData = musicService.data;
+        vm.arrayOfSongIds = [];
+        vm.arrayOfSongObjs = [];
         vm.searchQuery = '';
 
         // Declaration of private functions
         function _init() {
-            musicService.getMusicList();
+            musicService.getPersonalMusicList().then(function () {
+                vm.arrayOfSongIds = spotifyMusicModel.getArrayOfIds(vm.musicServiceData.personalMusicList);
+                spotifyService.getTracks(vm.arrayOfSongIds).then(function (response) {
+                    vm.arrayOfSongObjs = response.tracks;
+                });
+            });
         }
         _init();
 
         // Declaration of public functions
         function searchForMusic(song) {
             if (!vm.searchQuery ||
-                    (song.artistname.toLowerCase().indexOf(vm.searchQuery.toLowerCase()) !== -1) ||
-                    (song.songname.toLowerCase().indexOf(vm.searchQuery.toLowerCase()) !== -1)) {
+                    (song.artists[0].name.toLowerCase().indexOf(vm.searchQuery.toLowerCase()) !== -1) ||
+                    (song.name.toLowerCase().indexOf(vm.searchQuery.toLowerCase()) !== -1)) {
                 return true;
             }
             return false;
